69f8f57140dae014541d9e530675072d639033b4
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-20 14:52:14 +0200

    add export to types in index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export type Rating = 1 | 2 | 3 | 4 | 5;
+export type MembershipType = "free" | "premium";
+export type Genres = "fiction" | "non-fiction" | "novel" | "sci-fi" | "mytery" | "fantasy" | "children's literature" | "science" | "biography"
