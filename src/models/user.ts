dfbb7d7ad7285b5be216dff72f8006b28eccbc10
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-20 14:54:04 +0200

    add user class

---
+++ src/models/user.ts
@@ -0,0 +1,23 @@
+import { Book } from "./book";
+import { MembershipType } from "../types/index";
+
+export class User {
+    private _id: number;
+    private _name: string;
+    private _email: string;
+    private _borrowedBooks: Book[]; //(array of books)
+    private _readingList: number[]; //(array of book IDs)
+    private _membershipType: MembershipType;
+    private _joinedDate: Date;
+
+
+	constructor(name: string, email: string, membershipType: MembershipType) {
+		this._id = -1;
+        this._name = name;
+		this._email = email;
+		this._borrowedBooks = [];
+		this._readingList = [];
+        this._membershipType = membershipType;
+		this._joinedDate = new Date();
+	}
+}
