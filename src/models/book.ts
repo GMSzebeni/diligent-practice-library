b6fd566d42189aaf39bb29154a9fea6ca0cdaa88
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-20 16:33:49 +0200

    change ISBN to accept string instead of number and have getters for the library search method

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,5 +1,5 @@
 import { User } from "./user";
-import { Rating } from "../types/type";
+import { Rating, Genres } from "../types/type";
 
 export class Book {
     private _id: number;
@@ -7,18 +7,18 @@ export class Book {
     private _author: string;
     private _publisher: string;
     private _publicationYear: number;
-    private _ISBN: number;
+    private _ISBN: string;
     private _availability: boolean;
     private _borrower: User | null;
     private _borrowedDate: Date | null;
-    private _genre: string;
+    private _genre: Genres;
     private _rating: Rating | 0;
     private _reviews: string[];
     private _language: string;
 
 
-	constructor(title: string, author: string, publisher: string, publicationYear: number, ISBN: number, 
-                genre: string, language: string) {
+	constructor(title: string, author: string, publisher: string, publicationYear: number, ISBN: string, 
+                genre: Genres, language: string) {
         this._id = -1;
 		this._title = title;
 		this._author = author;
@@ -36,6 +36,22 @@ export class Book {
 
     public get id(): number {
 		return this._id;
+	}
+
+	public get title(): string {
+		return this._title;
+	}
+
+	public get author(): string {
+		return this._author;
+	}
+
+	public get ISBN(): string {
+		return this._ISBN;
+	}
+
+	public get genre(): Genres {
+		return this._genre;
 	}
 
     public set id(value: number) {
