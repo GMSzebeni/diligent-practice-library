6186065ca8a4ed846e9f8154c9f3f98562ba1dc4
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-20 16:32:54 +0200

    add two more books to the library

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,36 @@ export function createApp() {
     const library = new Library();
 
     try {
-        const book = library.add(new Book(
+        const book1 = library.add(new Book(
             "Coding with Me",
             "Jim Smith",
             "Great Publisher Inc.",
             2020,
-            123456789,
+            "123456789",
             "science",
             "Hungarian"
         ))
-        console.log("The book has been added to the library: ", book);
+        console.log(`${book1.title} has been added to the library.`);
+        const book2 = library.add(new Book(
+            "To Kill a Mockingbird",
+            "Harper Lee",
+            "J. B. Lippincott & Co.",
+            1960,
+            "9780060173227",
+            "fiction",
+            "English"
+        ))
+        console.log(`${book2.title} has been added to the library.`);
+        const book3 = library.add(new Book(
+            "A legnagyszerűbb könyv a nárcizmusról",
+            "Bánki György",
+            "Ab Ovo Kiadó",
+            2016,
+            "9786155353918",
+            "non-fiction",
+            "Hungarian"
+        ))
+        console.log(`${book3.title} has been added to the library.`);
     } catch(error) {
         if (error instanceof AppError) {
             console.error("AppError:", error.message);
