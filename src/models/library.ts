744c41688cc50b1dbf9dfeb4f16b144ff609ec19
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-21 11:56:21 +0200

    add lend method and update nextId and add methods to be generalized for use on both Books and Users

--- src/models/library.ts
+++ src/models/library.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../errors/app-error";
 import { Book } from "./book";
 import { User } from "./user";
 
@@ -6,8 +7,8 @@ export class Library {
     users: User[] = [];
     genres: string[] = ["fiction", "non-fiction", "novel", "sci-fi", "mytery", "fantasy", "children's literature", "science", "biography"];
 
-    public nextId(): number {
-        const ids =  this.books.map((book) => book.id);
+    public nextId(items: Book[] | User[]): number {
+        const ids = items.map((item) => item.id);
         if (ids.length === 0) {
             return 1;
         }
@@ -15,10 +16,17 @@ export class Library {
         return maxId + 1;
     }
 
-    public add(book: Book): Book {
-        book.id = this.nextId();
-        this.books.push(book);
-        return book;
+    public add(item: Book): Book;
+    public add(item: User): User;
+    public add(item: Book | User) {
+        if (item instanceof Book) {
+            item.id = this.nextId(this.books);
+            this.books.push(item);
+        } else if (item instanceof User) {
+            item.id = this.nextId(this.users);
+            this.users.push(item);
+        };
+        return item;
     }
 
     public search(searchText: Partial<Pick<Book, 'title' | 'author' | 'ISBN' | 'genre'>>): Book[] {
@@ -30,6 +38,22 @@ export class Library {
                 (!searchText.genre || book.genre.toLowerCase().includes(searchText.genre.toLowerCase()))
             )
         });
+    }
+
+    public lend(bookTitle: string, userEmail: string): string {
+        const bookToLend = this.books.find(book => book.title === bookTitle);
+        const userToLendTo = this.users.find(user => user.email === userEmail);
+        if (bookToLend && userToLendTo) {
+            userToLendTo.addToBorrowedBooks(bookToLend);
+            this.books.splice(this.books.indexOf(bookToLend), 1);
+            return `Book "${bookToLend.title}" has been lent to user "${userToLendTo.name} (${userToLendTo.email})"`
+        } else if (!bookToLend) {
+            throw new AppError(`A book with the title "${bookTitle}" was not found in the libraray database.`)
+        } else if (!userToLendTo) {
+            throw new AppError(`There is no such user in the library database: ${userEmail}`)
+        } else {
+            throw new AppError('Book could not be lent.')
+        }
     }
 }
 
